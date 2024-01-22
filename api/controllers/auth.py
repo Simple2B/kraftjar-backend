f13a5ebc30f9ba85990bcf11d25fb407e6355a3c
sa1am8 <tinkerino571@gmail.com>
2024-01-22 15:13:21 +0200

    ruff fix

--- api/controllers/auth.py
+++ api/controllers/auth.py
@@ -1,11 +1,12 @@
-from sqlalchemy.orm import Session
 from fastapi import HTTPException, status
+from sqlalchemy.orm import Session
+
 from app import schema as s
 
 
 def google_auth(data: s.GoogleAuth, db: Session) -> s.Token:
     """Logs in a user with Google"""
-    
+
     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Google auth not implemented")
 
 
