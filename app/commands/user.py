3e27c91788ae9deb8dfb4d017fb7f8cb791e188a
Varvara Shcherbyna <varvarashcherbyna7@gmail.com>
2024-02-23 10:57:27 +0200

    export_users_from_google_spreadsheets

--- app/commands/user.py
+++ app/commands/user.py
@@ -1,4 +1,6 @@
 import os
+import re
+import random
 import json
 from pathlib import Path
 
@@ -21,6 +23,16 @@ CFG = config()
 
 SCOPES = CFG.SCOPES
 SPREADSHEET_ID = CFG.SPREADSHEET_ID
+RANGE_NAME = "Users!A1:P"
+# rows name
+# ['id', 'Персонаж', 'Статус', 'Вид послуг', 'Область', 'Стать', 'Вік', 'Рейтинг', 'З нами вже', 'Робіт/замовлень', 'Опис', 'Імʼя', 'Прізвище', 'e-mail', 'phone', 'id']
+PERSON = "Персонаж"
+SERVICES_TYPE = "Вид послуг"
+REGION = "Область"
+NAME = "Імʼя"
+SURNAME = "Прізвище"
+EMAIL = "e-mail"
+PHONE = "phone"
 
 
 def export_users_from_google_spreadsheets(with_print: bool = True):
@@ -44,47 +56,73 @@ def export_users_from_google_spreadsheets(with_print: bool = True):
         service = build("sheets", "v4", credentials=credentials)
         sheets = service.spreadsheets()
 
-        # get all values from sheet
         # get all values from sheet Users
-        # result = sheets.values().get(spreadsheetId=SPREADSHEET_ID, range="Users!A1:O217").execute()
-        # values = result.get("values", [])
+        result = sheets.values().get(spreadsheetId=SPREADSHEET_ID, range=RANGE_NAME).execute()
+        values = result.get("values", [])
+
+        if not values:
+            log(log.INFO, "No data found")
+            return
+
+        # indexes of row values
+        PERSON_INDEX = values[0].index(PERSON)
+        PHONE_INDEX = values[0].index(PHONE)
+        EMAIL_INDEX = values[0].index(EMAIL)
+        SERVICES_TYPE_INDEX = values[0].index(SERVICES_TYPE)
+
+        # TODO: first  name and last name must be add to model User
+        NAME_INDEX = values[0].index(NAME)
+        SURNAME_INDEX = values[0].index(SURNAME)
+
+        # TODO: correct data region in google sheet
+        REGION_INDEX = values[0].index(REGION)
 
         with db.begin() as session:
             if not session.scalar(sa.select(m.Location)):
                 log(log.ERROR, "Locations table is empty")
                 log(log.ERROR, "Please run `flask export-regions` first")
                 raise Exception("Locations table is empty. Please run `flask export-regions` first")
-
             if not session.scalar(sa.select(m.Service)):
                 log(log.ERROR, "Services table is empty")
                 log(log.ERROR, "Please run `flask export-services` first")
                 raise Exception("Services table is empty. Please run `flask export-services` first")
 
-            for row in range(2, 217):
-                user_phone: str = sheets.values().get(spreadsheetId=SPREADSHEET_ID, range=f"Users!O{row}").execute()
+            # from 2 to 218
+            for row in values:
+                user_fullname = row[PERSON_INDEX]
+                if not user_fullname:
+                    log(log.INFO, "This row is empty")
+                    continue
+                user_phone: str = ""
+                if not row[PHONE_INDEX]:
+                    code = random.randint(63, 99)
+                    num = random.randint(100, 999)
+                    user_phone = f"380{code}1234{num}"
+                else:
+                    user_phone = row[PHONE_INDEX]
+
                 if session.scalar(sa.select(m.User).where(m.User.phone == user_phone)):
                     log(log.INFO, "User with phone [%s] already exists", user_phone)
                     continue
-                if not user_phone:
-                    for num in range(10, 99):
-                        user_phone = f"+3809912345{num}"
-                user_fullname = sheets.values().get(spreadsheetId=SPREADSHEET_ID, range=f"Users!A{row}").execute()
 
-                user_email = sheets.values().get(spreadsheetId=SPREADSHEET_ID, range=f"Users!N{row}").execute()
+                # TODO: add to model User firstname and lastname
+                user_firstname = row[NAME_INDEX]
+                # user_lastname = row[SURNAME_INDEX]
 
+                user_email: str = row[EMAIL_INDEX]
                 if not user_email:
-                    user_email = f"{user_fullname}@mail.com"
+                    user_email = f"{user_firstname.lower().strip()}@gmail.com"
+                user_password = "123"
 
-                user_password = "#" + user_fullname
                 new_user: m.User = m.User(
                     fullname=user_fullname,
                     phone=user_phone,
                     email=user_email,
                     password=user_password,
-                    # is_volunteer=user_is_volunteer,
+                    phone_verified=True,
                 )
 
-                # TODO: must be add services and location
+                # TODO: must be add location
                 # location_id =
                 # location: m.Location | None = session.scalar(
                 #     sa.select(m.Location).where(m.Location.id == location_id)
@@ -94,21 +132,25 @@ def export_users_from_google_spreadsheets(with_print: bool = True):
                 #     raise Exception(f"Location with id [{location_id}] not found")
                 # new_user.locations.append(location)
 
-                # service_id =
-                # service: m.Service | None = session.scalar(sa.select(m.Service).where(m.Service.id == service_id))
-                # if not service:
-                #     log(log.ERROR, "Service with id [%s] not found", service_id)
-                #     raise Exception(f"Service with id [{service_id}] not found")
-                # new_user.services.append(service)
+                user_services = row[SERVICES_TYPE_INDEX]
+                services_ids = re.findall(r"\d+", user_services)
+
+                for service_id in services_ids:
+                    service: m.Service | None = session.scalar(sa.select(m.Service).where(m.Service.id == service_id))
+                    if not service:
+                        log(log.ERROR, "Service with id [%s] not found", service_id)
+                        raise Exception(f"Service with id [{service_id}] not found")
+                    new_user.services.append(service)
 
                 session.add(new_user)
-                session.refresh(new_user)
                 if with_print:
                     log(log.INFO, f"Created user {new_user.fullname}")
 
     except HttpError as error:
         log(log.ERROR, "An error occurred: %s", error)
         raise Exception("An error occurred")
+
+    os.remove("token.json")
 
 
 MODULE_PATH = Path(__file__).parent
