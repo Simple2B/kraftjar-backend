e731ac34e3ab58073a26a88e723717f04faa373b
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2024-09-04 13:10:34 +0300

    deleted function

--- api/controllers/auth.py
+++ api/controllers/auth.py
@@ -4,10 +4,10 @@ from sqlalchemy.orm import Session
 from app import schema as s
 
 
-def google_auth(data: s.GoogleAuth, db: Session) -> s.Token:
-    """Logs in a user with Google"""
+# def google_auth(data: s.GoogleAuth, db: Session) -> s.Token:
+#     """Logs in a user with Google"""
 
-    raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Google auth not implemented")
+#     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Google auth not implemented")
 
 
 def apple_auth(data: s.AppleAuth, db: Session) -> s.Token:
