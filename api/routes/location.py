f00f096fab49780b96e17a53342765041fce5d41
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2024-09-17 15:35:33 +0300

    дщсфешщт

--- api/routes/location.py
+++ api/routes/location.py
@@ -18,3 +18,23 @@ location_router = APIRouter(prefix="/locations", tags=["Location"])
 )
 def get_locations(query: s.LocationsIn, db: Session = Depends(get_db)):
     return c.get_locations(query, db)
+
+
+@location_router.get(
+    "/address",
+    status_code=status.HTTP_200_OK,
+    response_model=s.LocationsOut,
+)
+def get_address(
+    query: str,
+    db: Session = Depends(get_db),
+):
+    """Returns the address"""
+    # find address by query
+
+    db_address: Sequence[m.Address] = db.scalars(sa.select(m.Address).where(m.Address.name == query)).all()
+
+    if not db_address:
+        return s.LocationsOut(lang="ua", locations=[], selected=[])
+
+    return
