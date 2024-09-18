3dc7686ad9a21592d9675f1f35fb33872125c9ea
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2024-09-18 22:30:17 +0300

    commands

--- app/commands/addresses.py
+++ app/commands/addresses.py
@@ -9,11 +9,11 @@ from app.database import db
 from app.logger import log
 
 MODULE_PATH = Path(__file__).parent
-JSON_FILE = MODULE_PATH / ".." / ".." / "data" / "address.json"
+JSON_FILE_ADDRESSES = MODULE_PATH / ".." / ".." / "data" / "address.json"
 
 
 def check_if_address_exists(address: s.AddressBase, session: sa.orm.Session):
-    """Check if region exists in db"""
+    """Check if address exists in db"""
 
     stmt = sa.select(m.Address).where(m.Address.line1 == address.line1)
     db_address = session.scalar(stmt)
@@ -23,10 +23,10 @@ def check_if_address_exists(address: s.AddressBase, session: sa.orm.Session):
 def export_addresses_from_json_file(with_print: bool = True):
     """Creates records in address table from json"""
 
-    with open(JSON_FILE, "r") as file:
-        file_data = s.AddressesFile.model_validate(json.load(file))
+    with open(JSON_FILE_ADDRESSES, "r") as file:
+        file_data_addresses = s.AddressesFile.model_validate(json.load(file))
 
-    addresses = file_data.addresses
+    addresses = file_data_addresses.addresses
     with db.begin() as session:
         for address in addresses:
             if check_if_address_exists(address, session):
@@ -40,6 +40,8 @@ def export_addresses_from_json_file(with_print: bool = True):
                 location_id=address.location_id,
                 street_id=address.street_id,
                 city_id=address.city_id,
+                street_type_ua=address.street_type_ua,
+                street_type_en=address.street_type_en,
             )
 
             session.add(address_db)
---
+++ app/commands/cities.py
@@ -0,0 +1,50 @@
+import json
+from pathlib import Path
+
+import sqlalchemy as sa
+
+from app import models as m
+from app import schema as s
+from app.database import db
+from app.logger import log
+
+MODULE_PATH = Path(__file__).parent
+JSON_FILE_CITIES = MODULE_PATH / ".." / ".." / "data" / "cities.json"
+
+
+def check_if_city_exists(city: s.City, session: sa.orm.Session):
+    """Check if city exists in db"""
+
+    stmt = sa.select(m.Settlement).where(m.Settlement.district_id == city.district_id)
+    db_city = session.scalar(stmt)
+    return db_city is not None
+
+
+def export_cities_from_json_file(with_print: bool = True):
+    """Creates records in city table from json"""
+
+    with open(JSON_FILE_CITIES, "r") as file:
+        file_data_cities = s.CitiesFile.model_validate(json.load(file))
+
+    cities = file_data_cities.cities
+    with db.begin() as session:
+        for city in cities:
+            if check_if_city_exists(city, session):
+                continue
+
+            city_db = m.Settlement(
+                location_id=city.location_id,
+                district_id=city.district_id,
+                city_id=city.city_id,
+                kt=city.kt,
+                type=s.Type(city.type),
+                name_ua=city.name_ua,
+                name_en=city.name_en,
+            )
+
+            session.add(city_db)
+            session.flush()
+            if with_print:
+                log(log.DEBUG, f"{city_db.id}: {city_db.name_ua}")
+
+    return
