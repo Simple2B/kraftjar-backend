4e775295adba44efe254d486223f8e67eab89f95
AlexAzalor <xy55xy.azalor@gmail.com>
2024-10-10 11:01:07 +0300

    fix

--- app/schema/job.py
+++ app/schema/job.py
@@ -247,7 +247,6 @@ class JobOutput(BaseModel):
     created_at: datetime
     location: LocationStrings | None = None
     services: list[Service]
-    is_favorite: bool
 
     model_config = ConfigDict(
         from_attributes=True,
