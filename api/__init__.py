b46fd87e620f9a7baccd695e242f3da909362aa2
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2024-11-10 21:55:42 +0200

    custom_generate_unique_id

--- api/__init__.py
+++ api/__init__.py
@@ -5,12 +5,15 @@ from fastapi_pagination import add_pagination
 
 from config import config
 
-from .utils import custom_generate_unique_id
+# from .utils import custom_generate_unique_id
 from .routes import router
 
 CFG = config()
 
-app = FastAPI(version=CFG.VERSION, generate_unique_id_function=custom_generate_unique_id)
+app = FastAPI(
+    version=CFG.VERSION,
+    # generate_unique_id_function=custom_generate_unique_id,
+)
 app.include_router(router)
 add_pagination(app)
 
