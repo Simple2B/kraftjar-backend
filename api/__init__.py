862acfa20d02ac80dd8ce04f50c06700d168005f
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2024-11-10 22:13:14 +0200

    deep linking

--- api/__init__.py
+++ api/__init__.py
@@ -1,25 +1,29 @@
 import json
-from fastapi import FastAPI, status
+from fastapi import APIRouter, FastAPI, status
 from fastapi.responses import RedirectResponse
 from fastapi_pagination import add_pagination
 
 from config import config
 
-# from .utils import custom_generate_unique_id
+from .utils import custom_generate_unique_id
 from .routes import router
 
 CFG = config()
 
+router_deep_linking = APIRouter(tags=["deep linking"])
+
 app = FastAPI(
     version=CFG.VERSION,
-    # generate_unique_id_function=custom_generate_unique_id,
+    generate_unique_id_function=custom_generate_unique_id,
 )
+
+app.include_router(router_deep_linking)
 app.include_router(router)
 add_pagination(app)
 
 
 # universal links for iOS (deep linking)
-@app.get(
+@router_deep_linking.get(
     "/.well-known/apple-app-site-association",
     status_code=status.HTTP_200_OK,
     response_model=dict,
@@ -31,7 +35,7 @@ def apple_app_link():
 
 
 # universal links for Android (deep linking)
-@app.get(
+@router_deep_linking.get(
     "/.well-known/assetlinks.json",
     status_code=status.HTTP_200_OK,
     response_model=dict,
